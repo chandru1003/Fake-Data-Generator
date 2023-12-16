2346393d7c3f2a05e7b1fba7dbbe27c2663b469f
chandru <chandrukongari@gmail.com>
2023-12-16 15:33:39 +0530

    Optimized

--- FakedataGenerator.py
+++ FakedataGenerator.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from faker import Faker
 
@@ -23,16 +24,23 @@ def generate_custom_fake_data(columns, rows):
     return pd.DataFrame(data)
 
 def save_to_file(dataframe, filename, file_format):
+    output_folder = "output"
+    os.makedirs(output_folder, exist_ok=True)
+
     if file_format == "csv":
-        dataframe.to_csv(f"{filename}.csv", index=False)
+        output_path = os.path.join(output_folder, f"{filename}.csv")
+        dataframe.to_csv(output_path, index=False)
     elif file_format == "xlsx":
-        dataframe.to_excel(f"{filename}.xlsx", index=False)
+        output_path = os.path.join(output_folder, f"{filename}.xlsx")
+        dataframe.to_excel(output_path, index=False)
     elif file_format == "json":
-        dataframe.to_json(f"{filename}.json", orient="records")
+        output_path = os.path.join(output_folder, f"{filename}.json")
+        dataframe.to_json(output_path, orient="records")
     else:
         print(f"Unsupported file format: {file_format}")
+        return
 
-    print(f"File '{filename}.{file_format}' created successfully.")
+    print(f"File '{output_path}' created successfully.")
 
 def main():
     # User input
