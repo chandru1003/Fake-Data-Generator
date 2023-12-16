125df8bb92b492fa68ab00635c1cd80be575c1e5
chandru <chandrukongari@gmail.com>
2023-12-16 14:28:46 +0530

    Extend file format options to include XLSX and JSON

--- FakedataGenerator.py
+++ FakedataGenerator.py
@@ -11,18 +11,28 @@ def generate_custom_fake_data(columns, rows):
                 data[column].append(fake.name())
             elif "email" in column.lower():
                 data[column].append(fake.email())
+            elif "country" in column.lower():
+                data[column].append(fake.country())
             elif "address" in column.lower():
                 data[column].append(fake.address())
             elif "date_of_birth" in column.lower():
-                data[column].append(fake.date_of_birth(minimum_age=18, maximum_age=65).strftime('%Y-%m-%d'))
+                data[column].append(fake.date_of_birth(minimum_age=18, maximum_age=65).strftime('%Y-%m-%d'))            
             else:
                 print(f"Invalid column name: {column}")
 
     return pd.DataFrame(data)
 
-def save_to_csv(dataframe, filename):
-    dataframe.to_csv(f"{filename}.csv", index=False)
-    print(f"File '{filename}.csv' created successfully.")
+def save_to_file(dataframe, filename, file_format):
+    if file_format == "csv":
+        dataframe.to_csv(f"{filename}.csv", index=False)
+    elif file_format == "xlsx":
+        dataframe.to_excel(f"{filename}.xlsx", index=False)
+    elif file_format == "json":
+        dataframe.to_json(f"{filename}.json", orient="records")
+    else:
+        print(f"Unsupported file format: {file_format}")
+
+    print(f"File '{filename}.{file_format}' created successfully.")
 
 def main():
     # User input
@@ -30,13 +40,14 @@ def main():
     columns_input = input("Enter column names (comma-separated): ").split(",")
     rows_input = int(input("Enter the number of rows: "))
     filename_input = input("Enter the filename (without extension): ")
+    file_format_input = input("Enter the file format (csv, xlsx, json): ").lower()
 
     # Generate custom fake data
     custom_fake_data = generate_custom_fake_data(columns_input, rows_input)
 
     # Save to CSV file
     if custom_fake_data is not None:
-        save_to_csv(custom_fake_data, filename_input)
+        save_to_file(custom_fake_data, filename_input, file_format_input)
 
 if __name__ == "__main__":
     main()
